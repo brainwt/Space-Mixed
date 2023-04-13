28a88bb7c7a5ef44ae192bfbbd325275677352c1
brainwt <brainwt@users.noreply.github.com>
2023-04-13 13:57:10 +0900

    Update fn_rand_X.m
    a

--- fn_rand_X.m
+++ fn_rand_X.m
@@ -5,8 +5,8 @@ function x_set = fn_rand_X(n_class, n_sample, dicim)
     for m=1:n_class
         x(:,m) = round(randi([0 10],n_sample,1)/10, dicim );
     end
-        x =  round( x./sum(x,2), dicim);
-        x = sort(x,2,'descend');
+    x =  round( x./sum(x,2), dicim);
+    x = sort(x,2,'descend');
         
     for m = 1:n_class
        n_list = [n_class:-1:1];
